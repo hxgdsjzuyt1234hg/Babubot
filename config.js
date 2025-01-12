086d47bf72abdac2fc60d7672dec7261cec32e74
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-01-12 20:38:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// BOT TELE
+module.exports = {
+  BOT_TOKEN: "7772786423:AAFF5hb0i7w_NVJ_GNN5Jah5ARJpkcUPQBo", // Token bot Telegram
+  OWNER_ID: "7865363008", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
